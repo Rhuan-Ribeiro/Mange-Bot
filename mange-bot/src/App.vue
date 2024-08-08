5f6eae83bfcff65443e15c315fb3ce02ceee98c1
Rhuan-Ribeiro <rhuan.augusto.ribeiro@gmail.com>
2024-08-08 19:39:27 -0300

    stylized app container

--- mange-bot/src/App.vue
+++ mange-bot/src/App.vue
@@ -23,6 +23,13 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 
-<style scoped>
+<style scoped lang="scss">
+#card-app {
+    height: 90%;
+    width: 80%;
+    background-color: var(--app-card-background);
+    border-radius: 1.5rem;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
+}
 
 </style>
